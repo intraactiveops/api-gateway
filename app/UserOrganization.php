6183f179c0c78a4356b140e316f4ee687fa728a6
johnenrick <johnenrickplenos@gmail.com>
2019-06-25 16:03:15 +0800

    Rename User Organization Fix

--- app/UserOrganization.php
+++ app/UserOrganization.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UserMembership extends GenericModel
+class UserOrganization extends GenericModel
 {
   public $validationRules = [
     "year_started" => 'numeric|min:1521|required'
